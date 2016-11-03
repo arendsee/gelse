bb54ba0a75cec0016a86881cb22b3b0aad0c60bc
Zebulun Arendsee <arendsee@iastate.edu>
2016-11-03 16:15:22 -0500

    Fix logic bug that selected last index

--- src/gelse.cpp
+++ src/gelse.cpp
@@ -8,11 +8,13 @@ Rcpp::IntegerVector c_gelse(Rcpp::LogicalMatrix mat)
 
     Rcpp::IntegerVector indices(N);
 
+    // set index to index of the first positive element in row (1-based)
     for(int i = 0; i < N; i++){
         indices[i] = NA_INTEGER;
         for(int j = 0; j < M; j++){
             if(mat(i,j)){
                 indices[i] = j+1; 
+                break;
             }
         }
     }
